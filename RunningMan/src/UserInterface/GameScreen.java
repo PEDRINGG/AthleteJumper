3484e71afeae70071d843cd342490174c8cdcbe7
AXACFA <92195092+AXACFA@users.noreply.github.com>
2021-10-09 17:15:23 +0800

    Update GameScreen.java

--- RunningMan/src/UserInterface/GameScreen.java
+++ RunningMan/src/UserInterface/GameScreen.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+// Sample comment.
 package UserInterface;
 import java.awt.Color;
 import java.awt.Graphics;
